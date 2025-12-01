90f03aae06c8e0a28149fa124f5f334d16ac15d1
Kenton Knelle <kken51915@gmail.com>
2025-12-01 10:11:58 -0500

    Module 1 Review

---
+++ Module_1_Review/Formulas.cs
@@ -0,0 +1,62 @@
+﻿namespace Module_1_Review;
+
+public class Formulas
+{
+    public double AreaOfACircle()
+    {
+        Console.WriteLine("What is the Radius of the circle?");
+        var radius = double.Parse(Console.ReadLine());
+        
+        var circleArea = (3.14 * (radius * radius));
+        
+        Console.WriteLine($"The area of the Circle is {circleArea}");
+
+        return circleArea;
+        
+    }
+
+    public static void AreaOfATriangle()
+    {
+        Console.WriteLine("What is the Base of the triangle?");
+        var userBase = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("What is the Height of the triangle?");
+        var userHeight = double.Parse(Console.ReadLine());
+        
+        var triangleArea = (0.5 * (userBase * userHeight));
+        Console.WriteLine($"The area of the triangle is {triangleArea}");
+    }
+
+    public static void AreaOfARectangle()
+    {
+        Console.WriteLine("What is the Length of the rectangle?");
+        var userLength = double.Parse(Console.ReadLine());
+        
+        Console.WriteLine("What is the Width of the rectangle?");
+        var userWidth = double.Parse(Console.ReadLine());
+        
+        var rectangleArea = (userLength * userWidth);
+        Console.WriteLine($"The area of the rectangle is {rectangleArea}");
+    }
+
+    public static void AreaOfASquare()
+    {
+        Console.WriteLine("What is the Side of the square?");
+        var userSide = double.Parse(Console.ReadLine());
+        
+        var squareArea = (userSide * userSide);
+        Console.WriteLine($"The area of the square is {squareArea}");
+    }
+
+
+    public void MethodRun()
+    {
+        AreaOfACircle();
+        AreaOfATriangle();
+        AreaOfARectangle();
+        AreaOfASquare();
+    }
+    
+}
+
+
---
+++ Module_1_Review/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Module_1_Review;
+
+class Program : Formulas
+{
+    
+    
+    public void Main(string[] args)
+    {
+        MethodRun();
+    }
+
+    
+}
